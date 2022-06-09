6d13b2d40c42b8918bb15c78bef653a841c8c533
Usuario <Usuario@Usuario-PC>
2022-06-09 15:41:23 -0500

    prueba 2

--- cv_ejercicio3/src/cv_ejercicio3/Cv_ejercicio3.java
+++ cv_ejercicio3/src/cv_ejercicio3/Cv_ejercicio3.java
@@ -10,6 +10,10 @@ public class Cv_ejercicio3 {
         System.out.println("version 1");
         System.out.println("version 2");
         System.out.println("version 3");
+        System.out.println("version 4");
+        System.out.println("version 5");
+        System.out.println("version 6");
+        System.out.println("version 7");
     }
     
 }
