52c6b18728a354faf83e26008c312d9d4e0c9674
Usuario <Usuario@Usuario-PC>
2022-06-09 15:35:08 -0500

    prueba 0

---
+++ cv_ejercicio3/src/cv_ejercicio3/Cv_ejercicio3.java
@@ -0,0 +1,13 @@
+
+package cv_ejercicio3;
+
+
+public class Cv_ejercicio3 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("version 0");
+        System.out.println("version 1");
+    }
+    
+}
